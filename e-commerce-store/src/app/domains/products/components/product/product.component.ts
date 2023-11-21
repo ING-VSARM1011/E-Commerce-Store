f4457c0d3b3e6f42067e5df98cc284200415dcc3
LAPTOP-VUQ1J6NP\Victor_Moreno <victorsahir1011@gmail.com>
2023-11-20 19:40:51 -0500

    Uso de Inputs en Angular: Envio de la información padre a hijo.

--- e-commerce-store/src/app/domains/products/components/product/product.component.ts
+++ e-commerce-store/src/app/domains/products/components/product/product.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 @Component({
@@ -8,6 +8,9 @@ import { CommonModule } from '@angular/common';
   templateUrl: './product.component.html',
   styleUrl: './product.component.css'
 })
+
 export class ProductComponent {
-  img = 'https://picsum.photos/640/640?r=' + Math.random()
+  @Input({ required: true }) img: string = '';
+  @Input({ required: true }) price: number = 0;
+  @Input({ required: true }) title: string = '';
 }
--- e-commerce-store/src/app/domains/products/pages/list/list.component.ts
+++ e-commerce-store/src/app/domains/products/pages/list/list.component.ts
@@ -9,6 +9,7 @@ import { ProductComponent } from './../../components/product/product.component'
   templateUrl: './list.component.html',
   styleUrl: './list.component.css'
 })
+
 export class ListComponent {
 
 }
