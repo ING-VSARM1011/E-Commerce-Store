520308ab2ef48579b86d3cfa4224048d5f0d51b2
LAPTOP-VUQ1J6NP\Victor_Moreno <victorsahir1011@gmail.com>
2023-11-22 20:33:50 -0500

    Creando componente de productos.

--- e-commerce-store/src/app/domains/products/pages/list/list.component.ts
+++ e-commerce-store/src/app/domains/products/pages/list/list.component.ts
@@ -1,6 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ProductComponent } from './../../components/product/product.component'
+import { Product } from '../../../shared/models/product.model';
 
 @Component({
   selector: 'app-list',
@@ -11,6 +12,29 @@ import { ProductComponent } from './../../components/product/product.component'
 })
 
 export class ListComponent {
+
+  products = signal<Product[]>([]);
+
+  constructor() {
+    const initProducts: Product[] = [
+      {
+        id: Date.now(),
+        title: 'Product 1',
+        price: 100,
+        image: 'https://picsum.photos/640/640?r=23',
+        creationAt: new Date().toISOString()
+      },
+      {
+        id: Date.now(),
+        title: 'Product 2',
+        price: 200,
+        image: 'https://picsum.photos/640/640?r=24',
+        creationAt: new Date().toISOString()
+      }
+    ];
+    this.products.set(initProducts);
+  }
+
   fromChild(event: String) {
     console.log('Estamos en el padre');
     console.log(event);
