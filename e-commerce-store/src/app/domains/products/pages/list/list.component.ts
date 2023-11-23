793b320d90b1d69fe81fd1d7d855696fc4f7dd40
LAPTOP-VUQ1J6NP\Victor_Moreno <victorsahir1011@gmail.com>
2023-11-22 20:51:49 -0500

    Creando el header.

--- e-commerce-store/src/app/domains/products/pages/list/list.component.ts
+++ e-commerce-store/src/app/domains/products/pages/list/list.component.ts
@@ -1,12 +1,13 @@
 import { Component, signal } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ProductComponent } from './../../components/product/product.component'
+import { HeaderComponent } from '../../../shared/components/header/header.component';
 import { Product } from '../../../shared/models/product.model';
 
 @Component({
   selector: 'app-list',
   standalone: true,
-  imports: [CommonModule, ProductComponent],
+  imports: [CommonModule, ProductComponent, HeaderComponent],
   templateUrl: './list.component.html',
   styleUrl: './list.component.css'
 })
@@ -29,6 +30,20 @@ export class ListComponent {
         title: 'Product 2',
         price: 200,
         image: 'https://picsum.photos/640/640?r=24',
+        creationAt: new Date().toISOString()
+      },
+      {
+        id: Date.now(),
+        title: 'Product 3',
+        price: 300,
+        image: 'https://picsum.photos/640/640?r=25',
+        creationAt: new Date().toISOString()
+      },
+      {
+        id: Date.now(),
+        title: 'Product 4',
+        price: 400,
+        image: 'https://picsum.photos/640/640?r=26',
         creationAt: new Date().toISOString()
       }
     ];
